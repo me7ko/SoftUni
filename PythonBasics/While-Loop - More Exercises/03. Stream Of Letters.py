9f319296673899ffd8eec98366bef7c537d78706
metko61 <61345845+metko61@users.noreply.github.com>
2022-10-12 16:48:47 +0300

    Python Basics

--- PythonBasics/While-Loop - More Exercises/03. Stream Of Letters.py
+++ PythonBasics/While-Loop - More Exercises/03. Stream Of Letters.py
@@ -1,7 +1,7 @@
 c = 0
 o = 0
 n = 0
-first_word = ""
+current_word = ""
 final_word = ""
 
 symbol = input()
@@ -10,20 +10,20 @@ while symbol != "End":
         if symbol == "c":
             c += 1
             if c >= 2:
-                first_word += symbol
+                current_word += symbol
         elif symbol == "o":
             o += 1
             if o >= 2:
-                first_word += symbol
+                current_word += symbol
         elif symbol == "n":
             n += 1
             if n >= 2:
-                first_word += symbol
+                current_word += symbol
         else:
-            first_word += symbol
+            current_word += symbol
         if c >= 1 and o >= 1 and n >= 1:
-            final_word += first_word + " "
-            first_word = ""
+            final_word += current_word + " "
+            current_word = ""
             c = 0
             o = 0
             n = 0
